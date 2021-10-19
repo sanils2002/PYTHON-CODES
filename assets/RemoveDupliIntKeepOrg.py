71d4431b6e25da12deef281a5c69ff3e0c29d9a1
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-19 18:02:09 +0530

    Update RemoveDupliIntKeepOrg.py

--- assets/RemoveDupliIntKeepOrg.py
+++ assets/RemoveDupliIntKeepOrg.py
@@ -1,3 +1,13 @@
+import os
+from time import sleep
+def screen_clear():
+   if os.name == 'posix':
+      _ = os.system('clear')
+   else:
+      _ = os.system('cls')
+sleep(1)
+screen_clear()
+
 def Remove(duplicate):
     final_list = []
     for num in duplicate:
