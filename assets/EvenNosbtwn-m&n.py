195658ff48015802bb5a8b9806064a4cf9c5c14f
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-19 18:03:33 +0530

    Update EvenNosbtwn-m&n.py

--- assets/EvenNosbtwn-m&n.py
+++ assets/EvenNosbtwn-m&n.py
@@ -18,4 +18,5 @@ for i in range(x, y+1):
     s = str(i)
     if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0):
         items.append(s)
+print("Even Numbers:")         
 print( ",".join(items))
