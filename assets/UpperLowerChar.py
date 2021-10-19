e0461532ef2891cfc7a17a0b2bc5e9806c701ff9
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-19 17:17:19 +0530

    Create UpperLowerChar.py

---
+++ assets/UpperLowerChar.py
@@ -0,0 +1,15 @@
+def string_test(s):
+    d={"UPPER_CASE":0, "LOWER_CASE":0}
+    for c in s:
+        if c.isupper():
+           d["UPPER_CASE"]+=1
+        elif c.islower():
+           d["LOWER_CASE"]+=1
+        else:
+           pass
+    print ("Original String : ", s)
+    print ("No. of Upper Case Characters : ", d["UPPER_CASE"])
+    print ("No. of Lower Case Characters : ", d["LOWER_CASE"])
+
+string=input("Enter String:")
+string_test(string)
