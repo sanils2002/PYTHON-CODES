2f5048816e19854a2e870b9693ec3a05b156a037
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-19 17:26:19 +0530

    Update RemoveDupliIntKeepOrg.py

--- assets/RemoveDupliIntKeepOrg.py
+++ assets/RemoveDupliIntKeepOrg.py
@@ -1,15 +1,9 @@
-def removeDuplicate(li):
-    newli=[]
-    seen=set()
-    for item in li:
-        if item not in seen:
-            seen.add(item)
-            newli.append(item)
-            return newli
-
-li=[]
-li=list(map(int, input().split()))
-x=removeDuplicate(li)
-
-for i in x:
-    print(i, end=" ")
+def Remove(duplicate):
+    final_list = []
+    for num in duplicate:
+        if num not in final_list:
+            final_list.append(num)
+    return final_list
+     
+duplicate = [2, 4, 10, 20, 5, 2, 20, 4]
+print(Remove(duplicate))
