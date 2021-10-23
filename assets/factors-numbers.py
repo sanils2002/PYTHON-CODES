5e4d1f84fe2af85ef9e7e65ec2d0f948314e48ca
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-23 16:25:24 +0530

    Create factors-numbers.py

---
+++ assets/factors-numbers.py
@@ -0,0 +1,21 @@
+import os
+from time import sleep
+def screen_clear():
+   if os.name == 'posix':
+      _ = os.system('clear')
+   else:
+      _ = os.system('cls')
+sleep(1)
+screen_clear()
+
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = int(input("Enter the number:"))
+
+screen_clear()
+
+print_factors(num)
