b75eaa4190ef624e8b3ed141b359a32370ea69e8
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2021-10-26 00:23:00 +0530

    Update Moneyclass.py

--- assets/Moneyclass.py
+++ assets/Moneyclass.py
@@ -1,6 +1,6 @@
 # Design and implement a Money class that stores monetary values in dollars and cents. Special method __init__ should have the following function header,
 # def __init__(self, dollars, cents)
-#Include special method __repr__ (__str__) for displaying values in dollars and cents: $ 0.45, $ 1.00, $ 1.25. Also include special method __add__, and three 
+# Include special method __repr__ (__str__) for displaying values in dollars and cents: $ 0.45, $ 1.00, $ 1.25. Also include special method __add__, and three 
 # getter methods that each provide the monetary value in another currency. Choose any three currencies to convert to.
 
 import os
